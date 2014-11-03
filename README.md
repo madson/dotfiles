cf9186ba1ee24ee7372e5561259a8066f9ba52fd
Madson Cardoso <madson.cunha@buscapecompany.com>
2014-11-03 16:29:54 -0200

    Added setup scripts

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ dotfiles
 ========
 
 My ~/.* files
+
+## How to install
+
+    $ curl -L https://raw.github.com/tinogomes/rcfiles/master/install.sh | sh
+
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd ~
+
+if [ ! -d ~/.dotfiles ]; then
+  git clone git@github.com:madson/dotfiles.git .dotfiles || exit 1
+fi
+
+if [ ! -d ~/.dotfiles ]; then
+  echo "You need to clone dotfiles repository"
+  exit 1
+fi
+
+if [ -f ~/.bash_profile ]
+then
+  echo "Backing up your ~/.bash_profile file into ~/.bash_profile_original"
+  cp ~/.bash_profile ~/.bash_profile_original
+
+	echo "Changing .bash_profile..."
+	cat bash_init >> ~/.bash_profile
+fi
+
+echo "Creating links..."
+for file in $(ls bash_inc*)
+do
+  ln -s $file ~/.$file
+done
+
+cd -
+
+echo "Done."
+
