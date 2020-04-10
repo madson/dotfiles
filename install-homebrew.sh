11a34f39a04eb45b79ec18bb0beacedec3a04b09
Madson Cardoso <madsonmac@gmail.com>
2020-04-10 08:29:53 -0700

    Add install-homebrew.sh

---
+++ install-homebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install go
+brew install terraform
+brew install httpie
+brew install rbenv
